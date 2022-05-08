7d62f4b84c997c5fd7f8b37d2e4d75bb3e7b400c
fawzy ahmed <fawzy.fawzy46@gmail.com>
2022-05-08 20:40:25 +0200

    updates

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, NgModule, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,13 +14,30 @@ import { ConfirmPasswordValidatorDirective } from './shared/confirm-password-val
 
 export class SignUpFormComponent implements OnInit {
 
-  constructor(private router: Router, private SignupService: SignupService) { }
+  constructor(private router: Router, private SignupService: SignupService, private http: HttpClient) { }
+
+  isValidSignup = true;
+  url = 'http://localhost:8080/signup';
 
   ngOnInit(): void {
   }
   onSubmit(form: NgForm)
   {
-    this.SignupService.AddUser(form);
+    /*this.SignupService.AddUser(form);
+    this.isValidSignup = this.SignupService.isValidSignup;
+    console.log("signup-form + " + this.SignupService.isValidSignup);*/
+    const PostData = {
+      name: form.value.name,
+      email: form.value.email,
+      password: form.value.password
+    }
+
+    this.http.post(this.url,PostData).subscribe(responseData => {
+      this.isValidSignup = !!responseData;
+      if(this.isValidSignup) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
 }
--- src/app/signup.service.ts
+++ src/app/signup.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class SignupService {
 
   url = 'http://localhost:8080/signup';
+  isValidSignup = true;
   AddUser(form: NgForm) {
     const PostData = {
       name: form.value.name,
       email: form.value.email,
       password: form.value.password
     }
-    console.log(PostData);
+
     this.http.post(this.url,PostData).subscribe(responseData => {
-      console.log(responseData);
+      this.isValidSignup = !!responseData;
+      console.log(this.isValidSignup);
     });
   }
 
